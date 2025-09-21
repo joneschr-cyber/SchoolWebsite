99662cdd933c1f3f266b8d2ee8860b189dd088a5
joneschr-cyber <joneschr@wsc.school.nz>
2025-09-21 22:56:17 +1200

    First commit

---
+++ Script.js
@@ -0,0 +1,322 @@
+function filterItem() {
+  let ids = ["Stand1", "Stand2", "Stand3", "Stand4", "Stand5", "Stand6", "Stand7", "Stand8"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+function filterItem1() {
+  let ids = ["Aquariums", "Aquariums2", "Aquariums3", "Aquariums4", "Aquariums5", "Aquariums6", "Aquariums7",];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+
+document.getElementById("toggleBtn").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+document.getElementById("toggleBtn2").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem2() {
+  let ids = ["Betta Fish", "FemaleBetta", "CherryShrimp", "WhiteCloudMinnows", "NeonTetras", "CardinalTetras", "SalmonShark", "AmanoShrimp", "PeppermintShrimp", "ParadiseFish", "PeaPuffer", "GreenSpottedPuffer", "PorcupinePuffer", ];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn3").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem4() {
+  let ids = ["CommonGoldFish", "OrandaGoldfish", "CherryShrimp", "WhiteCloudMinnows", "NeonTetras", "CardinalTetras", "SalmonShark", "AmanoShrimp", "PeppermintShrimp", "PeaPuffer", "GreenSpottedPuffer", "PorcupinePuffer", ];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn4").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem5() {
+  let ids = ["CommonGoldFish", "OrandaGoldfish", "Betta Fish", "WhiteCloudMinnows", "NeonTetras", "CardinalTetras", "SalmonShark", "FemaleBetta", "ParadiseFish", "PeaPuffer", "GreenSpottedPuffer", "PorcupinePuffer", ];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn5").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem6() {
+  let ids = ["CommonGoldFish", "OrandaGoldfish", "Betta Fish", "SalmonShark", "FemaleBetta", "ParadiseFish", "PeaPuffer", "GreenSpottedPuffer", "PorcupinePuffer", "AmanoShrimp", "PeppermintShrimp", "CherryShrimp"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn6").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+
+function filterItem7() {
+  let ids = ["CommonGoldFish", "WhiteCloudMinnows", "NeonTetras", "CardinalTetras", "OrandaGoldfish", "Betta Fish", "FemaleBetta", "ParadiseFish", "PeaPuffer", "GreenSpottedPuffer", "PorcupinePuffer", "AmanoShrimp", "PeppermintShrimp", "CherryShrimp"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn7").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+
+function filterItem8() {
+  let ids = ["CommonGoldFish", "WhiteCloudMinnows", "NeonTetras", "CardinalTetras", "OrandaGoldfish", "Betta Fish", "FemaleBetta", "ParadiseFish", "AmanoShrimp", "PeppermintShrimp", "CherryShrimp", "SalmonShark"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+
+function filterItem9() {
+  let ids =["SpondgeFilter1", "SpondgeFilter2", "SpondgeFilter3", "Hang-OnFilter1", "Hang-OnFilter2", "Hang-OnFilter3", "FilterMedia1", "FilterMedia2", "FilterMedia3"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+
+function filterItem10() {
+  let ids =["CanisterFilter1", "CanisterFilter2", "Hang-OnFilter1", "Hang-OnFilter2", "Hang-OnFilter3", "FilterMedia1", "FilterMedia2", "FilterMedia3"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem11() {
+  let ids =["CanisterFilter1", "CanisterFilter2", "SpondgeFilter1", "SpondgeFilter2", "SpondgeFilter3", "FilterMedia1", "FilterMedia2", "FilterMedia3"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+function filterItem12() {
+  let ids =["CanisterFilter1", "CanisterFilter2", "Hang-OnFilter1", "Hang-OnFilter2", "Hang-OnFilter3", "SpondgeFilter1", "SpondgeFilter2", "SpondgeFilter3"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+
+
+function filterItem13() {
+  let ids =["Diftwood1", "Decorations1", "Decorations2"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+
+function filterItem14() {
+  let ids =["Rocks1", "Rocks2", "Rock3", "Decorations1", "Decorations2"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+function filterItem15() {
+  let ids =[ "Gravel1", "Gravel2"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+function filterItem16() {
+  let ids =["Sand1", "Sand2", "Sand3", "Sand4"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem17() {
+  let ids =["Rooted1", "Floaters1", "Mossess1"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem18() {
+  let ids =["Epiphytes1", "Epiphytes2", "Floaters1", "Mossess1"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
+
+
+function filterItem19() {
+  let ids =["Epiphytes1", "Epiphytes2", "Rooted1"];
+  ids.forEach(id => {
+    let item = document.getElementById(id);
+    if (item) {
+      item.classList.toggle("hidden");
+    }
+  });
+}
+
+document.getElementById("toggleBtn8").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
+
